97642265d944f6e57afbc9373a4785b4e6ea9756
marialago <50707572+marialago@users.noreply.github.com>
2020-06-05 15:45:40 -0300

    inicio do backend

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const server = express();
+//configurar pasta publica
+server.use(express.static("public"));
+//utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+//configurar caminhos da aplicação/ pag inicial
+// req: requisição,pedido / res: resposta
+server.get("/", (req, res) => {
+  return res.render("index.html", {title: "Um titulo"});
+});
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
+  });
+//ligar servidor
+server.listen(3000);
